b837f35bb0f62f04baf504031c449c3cdcd67fa7
fRIdRIeH <wladwaliullin@yandex.ru>
2026-01-26 08:24:45 +0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'fRIdRIeH Test'
+copyright = '2026, Vladislav Valiullin'
+author = 'Valiullin'
 
 release = '0.1'
-version = '0.1.0'
+version = '0.1.1'
 
 # -- General configuration
 
